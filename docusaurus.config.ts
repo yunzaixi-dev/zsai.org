58b92d2a06c139cda42e26b641c770c567bb5d31
zsai-org <x@zsai.org>
2025-03-30 02:17:19 +0800

    :zap: sitemap url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,7 +10,7 @@ const config: Config = {
   favicon: 'img/logo.ico',
 
   // Set the production url of your site here
-  url: 'https://www.zsai.zaixi.com',
+  url: 'https://zsai.org',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
